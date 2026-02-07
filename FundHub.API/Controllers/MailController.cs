0c7460001247acff0b7606b1c25357ed1a6891df
devostafa <mostafa.maher98@gmail.com>
2026-02-07 11:56:01 +0200

    Renamed MailController's sending action to "send" instead of "sendmail"

--- FundHub.API/Controllers/MailController.cs
+++ FundHub.API/Controllers/MailController.cs
@@ -14,7 +14,7 @@ public class MailController : BaseController
         _mailservice = mailservice;
     }
     
-    [HttpPost("sendmail")]
+    [HttpPost("send")]
     public async Task<bool> SendMail(MailRequest mailtosend)
     {
         return await _mailservice.SendMail(mailtosend);
